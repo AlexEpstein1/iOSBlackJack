6a8f637b78dbd2b9c144f4029ac672ab6d27cb61
Sam Noyes <snoyes1234@gmail.com>
2020-04-01 16:53:05 -0400

    Test commit

---
+++ HoldEm/HoldEm/MenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HoldEm
+//
+//  Created by Matt Finch on 4/1/20.
+//  Copyright © 2020 Duke University. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var singleplayer: UIButton!
+    @IBOutlet weak var multiplayer: UIButton!
+    @IBOutlet weak var instructions: UIButton!
+    @IBOutlet weak var settings: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
