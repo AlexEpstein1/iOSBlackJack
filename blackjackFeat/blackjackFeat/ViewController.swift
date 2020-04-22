d1a836bebcdfb2e3a98bda69af3bf11e56d3903c
Alex Epstein <ape6@duke.edu>
2020-04-21 20:01:19 -0400

    fixing merge

---
+++ blackjackFeat/blackjackFeat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  blackjackFeat
+//
+//  Created by Alex Epstein on 4/21/20.
+//  Copyright © 2020 Alex Epstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
